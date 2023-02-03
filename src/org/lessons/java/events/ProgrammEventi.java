ede7dd348851405d2cf763ae5942bef25386b0b2
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-03 17:05:27 +0100

    Definizione classe ProgrammEventi

---
+++ src/org/lessons/java/events/ProgrammEventi.java
@@ -0,0 +1,16 @@
+package org.lessons.java.events;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProgrammEventi {
+
+	private String title;
+	private List<Evento> events;
+
+	public ProgrammEventi(String _title) {
+		this.title = _title;
+		this.events = new ArrayList<Evento>();
+	}
+
+}
