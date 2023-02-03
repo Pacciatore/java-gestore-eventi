f9a791c7e356fab47f061e272a6f36ed20f585c0
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-03 17:52:08 +0100

    Completamento milestone bonus

--- src/org/lessons/java/events/Main.java
+++ src/org/lessons/java/events/Main.java
@@ -1,13 +1,40 @@
 package org.lessons.java.events;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		preBonus();
+		// preBonus();
+
+		ProgrammEventi eventsList = new ProgrammEventi("Concerti");
+
+		Evento event = new Evento("Lollo", LocalDate.parse("2023-05-28"), 10);
+		eventsList.addEvent(event);
+		Evento event1 = new Evento("Pallo", LocalDate.parse("2023-03-28"), 20);
+		eventsList.addEvent(event1);
+		Evento event2 = new Evento("Lallo", LocalDate.parse("2023-02-28"), 15);
+		eventsList.addEvent(event2);
+		Evento event3 = new Evento("Frollo", LocalDate.parse("2023-03-28"), 55);
+		eventsList.addEvent(event3);
+
+		System.out.println("Sono presenti " + eventsList.getEventsNumber() + " eventi in programma.");
+
+		LocalDate dateFilter = LocalDate.parse("2023-03-28");
+		List<Evento> filteredEvents = eventsList.getEventsByDate(dateFilter);
+
+		System.out.println("\nIn data " + dateFilter + " sono presenti i seguenti eventi:");
+
+		for (Evento filteredEvent : filteredEvents) {
+			System.out.println(filteredEvent.toString());
+		}
+
+		System.out.println();
+
+		System.out.println(eventsList.eventsList());
 
 	}
 
--- src/org/lessons/java/events/ProgrammEventi.java
+++ src/org/lessons/java/events/ProgrammEventi.java
@@ -2,6 +2,8 @@ package org.lessons.java.events;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ProgrammEventi {
@@ -36,6 +38,26 @@ public class ProgrammEventi {
 		}
 
 		return eventsByDate;
+	}
+
+	public String eventsList() {
+
+		// Dichiaro un nuovo comparatore su cui basare il riordino (sort)
+		Collections.sort(this.events, new Comparator<Evento>() {
+			// Il risultato della comparazione viene restituito come un intero
+			public int compare(Evento e1, Evento e2) {
+				return e1.getDate().compareTo(e2.getDate());
+			}
+		});
+
+		StringBuilder eventsListBuilder = new StringBuilder();
+
+		for (Evento event : this.events) {
+			eventsListBuilder.append(event.toString() + "\n");
+		}
+
+		return this.title + "\n" + eventsListBuilder;
+
 	}
 
 }
